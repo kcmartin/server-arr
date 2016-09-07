d6f5c106d42371296f9f28107a15d47e03e4257f
kcmartin <kcmartin@users.noreply.github.com>
2016-09-06 18:15:19 -0700

    Added duplicate check and error handling to Authentication controller

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,17 +1,31 @@
 const User = require('../models/user');
 
 exports.signup = function(req, res, next) {
+  // pull email and password off request body
   const email = req.body.email;
   const password = req.body.password;
 
   // See if a user with the given email exists
   User.findOne({ email: email }, function(err, existingUser) {
+    if (err) { return next(err); }
 
+    // If a user with the email DOES exist return an error
+    if (existingUser) {
+      return res.status(422).send({ error: 'Email is in use' });
+    }
+
+    // If a user with the email DOES NOT exist, create and save user
+    const user = new User({
+      email: email,
+      password: password
+    });
+
+    // now save record to db
+    user.save(function(err) {
+      if (err) { return next(err); }
+
+    // Respond to request indicating the user was created
+      res.json({ success: true });
+    });
   });
-
-  // If a user with the email DOES exist return an error
-
-  // If a user with the email DOES NOT exist, create and save user
-
-  // Respond to request indicating the user was created
 }
